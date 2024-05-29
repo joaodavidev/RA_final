460516a7059ff2280f71c779749c4f5e0be448dc
João Davi da Cruz Garbin <jdcruzgarbin@gmail.com>
2024-05-28 22:08:37 -0300

    Esboço primario cardapio

--- Cafeteria.py
+++ Cafeteria.py
@@ -1,3 +1,11 @@
-#Cafeteria 
-Cardapio = []
-Estoque = {}
+#Cafeteria
+#Ravaneda -> Alterar itens do cardapio
+#João Davi -> Excluir itens do cardapio
+#Lyra -> Adicionar itens ao cardapio
+#Gustavo -> Buscar itens no cardapio
+#Guilherme -> Listar todos os itens do cardapio e ponto extra
+
+entradas = {'entradas':['Pão de queijo','Cookies','Brownie','Donnuts']}
+bebidas = {'bebidas':['Capuccino','Milkshake','Expresso','Água','Chá']}
+principal = {'principal':['Tapioca','Bauru','Omelete']}
+preco = {'Pão de queijo':10,'Cookies':10,'Brownie':10,'Donnuts':10,'Chá':10,'Água':10}
